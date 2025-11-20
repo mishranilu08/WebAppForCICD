e07deaa528b324b6b321e1300f1e95888ec3f80a
Ashish <ashish.mishra@appliedsolartechnologies.com>
2025-11-20 16:27:15 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppForCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
